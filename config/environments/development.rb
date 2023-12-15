d72dfbef78e73cdcc0a762be13eafee36dce6f56
veeren08 <veerendragupta52525@gmail.com>
2023-12-15 13:58:48 +0530

    update code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+  config.assets.compile = true
 
 
   config.action_mailer.perform_caching = false
