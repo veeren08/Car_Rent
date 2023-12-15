d8450f44e79e69856c85ba833d1e49482ca3a9b5
veeren08 <veerendragupta52525@gmail.com>
2023-12-15 13:24:46 +0530

    update code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2021_03_18_071018) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_15_075403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,9 +30,16 @@ ActiveRecord::Schema[7.1].define(version: 2021_03_18_071018) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", precision: nil, null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -107,6 +114,7 @@ ActiveRecord::Schema[7.1].define(version: 2021_03_18_071018) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "reservations", "users"
   add_foreign_key "reservations", "vehicles"
   add_foreign_key "vehicles", "users"
