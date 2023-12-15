cadb230eb57822c2864ac2d07b8e98693905a0b7
veeren08 <veerendragupta52525@gmail.com>
2023-12-15 13:07:42 +0530

    Minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    # include LoadAndAuthorizeResource
-    protected
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_avatar, :email, :first_name, :last_name, :dateofbirth, :mobile, :city, :country, :zipcode, :address, :password])
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:profile_avatar, :email, :first_name, :last_name, :dateofbirth, :mobile, :city, :country, :zipcode, :address, :current_password)}
-      end
+  protect_from_forgery with: :null_session
 
-    def after_sign_in_path_for(resource)
-      root_path
-    end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # include LoadAndAuthorizeResource
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_avatar, :email, :first_name, :last_name, :dateofbirth, :mobile, :city, :country, :zipcode, :address, :password])
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:profile_avatar, :email, :first_name, :last_name, :dateofbirth, :mobile, :city, :country, :zipcode, :address, :current_password)}
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end 
